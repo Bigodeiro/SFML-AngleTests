658971bd2aa194606477dd649be40467e4823798
Bigodeiro <enzobigber@gmail.com>
2023-06-09 19:28:24 -0300

    Medição de angulo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <iomanip>
 #include <cmath>
 #include <math.h>
 
@@ -44,6 +45,21 @@ int main()
         sf::Vertex(sf::Vector2f(0,0))
     };
 
+    //*Texto do angulo
+    sf::Font fonte;
+    if (!fonte.loadFromFile("src/fonts/arial.TTF"))
+    {
+        std::cout << "azar kk" << std::endl;
+
+        system("pause");
+    }
+    sf::Text anguloTXT;
+    anguloTXT.setFont(fonte);
+    anguloTXT.setCharacterSize(20);
+    anguloTXT.setPosition(15,15);
+    anguloTXT.setString("teste");
+    anguloTXT.setFillColor(sf::Color(240, 99, 32));
+
 
     while (window.isOpen())//! cada iteração aqui é um frame
     {
@@ -63,18 +79,39 @@ int main()
     //!codigo a ser executado todo frame
     window.clear();
 
-    //* Atualizar angulação do ponteiro, utilizando semelhança de triangulo 
+    //* Atualizar angulação do ponteiro, utilizando semelhança de triangulo
     float mousePosX = sf::Mouse::getPosition(window).x - WIDTH/2;
     float mousePosY = sf::Mouse::getPosition(window).y - HEIGTH/2;
     float coeficienteAngularFuncPonteiro = (0 - mousePosY) / (0 - mousePosX);
     angPointer[1].position.x = (mousePosX * unitCircle.getRadius()) / sqrt( pow(mousePosX, 2) + pow(mousePosY, 2));
     angPointer[1].position.y = (mousePosY * unitCircle.getRadius()) / sqrt( pow(mousePosX, 2) + pow(mousePosY, 2));
+    //A mesma coisa para o texto so que menor 
+    anguloTXT.setPosition(((mousePosX * (unitCircle.getRadius())/4 ) / sqrt( pow(mousePosX, 2) + pow(mousePosY, 2)))+8, (mousePosY * (unitCircle.getRadius())/4 ) / sqrt( pow(mousePosX, 2) + pow(mousePosY, 2)));
+    
+    //*Calculo e atualização do angulo
+    float ang = std::abs(atan(mousePosY/mousePosX));
+    ang = ang * (180.0/3.141592653589793238463);
+    if(mousePosX < 0 && mousePosY < 0)
+    {
+        ang = 90 + 90 - ang;
+    }
+    else if(mousePosX < 0 && mousePosY > 0)
+    {
+        ang = 180 + ang;
+    }
+    else if(mousePosX > 0 && mousePosY > 0)
+    {
+        ang = 270 + 90 - ang;
+    }
+
+    anguloTXT.setString(std::to_string(ang));
 
     //? renderiza algo para a tela, modificando-a
     window.draw(unitCircle);
     window.draw(abscissas, 2, sf::Lines);
     window.draw(ordenadas, 2, sf::Lines);
     window.draw(angPointer, 2, sf::Lines);
+    window.draw(anguloTXT);
 
     //? recarrega a tela, fazendo com que as mudanças sejam efetivadas
     window.display();
